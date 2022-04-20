038349553ea3d4ca55cd8a4dcfb79ea479a4eea5
LucienZ <lucienzuber@gmail.com>
2022-04-20 13:30:02 +0200

    fix(Run): changed running of the code

--- SelfPiBox.py
+++ SelfPiBox.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
+import time
 from threading import Thread
 
+from code.Camera import Camera
 from code.Configuration import Configuration
 from code.UserInterface import UserInterface
 
 
 def main(threadName, *args):
-    Configuration.set_up(with_gpio=False, with_pygame=False)
+    Configuration.set_up(with_gpio=False, with_pygame=True)
     UserInterface.display_image(file=Configuration.EVENT_IMAGE)
-    UserInterface.run_self_pi_box()
+    while True:
+        Camera.wait_for_event()
+        UserInterface.display_image(file=Configuration.EVENT_IMAGE)
+        time.sleep(0.2)
+        Camera.take_picture()
+
 
 # launch the main thread
 Thread(target=main, args=('Main', 1)).start()
--- code/UserInterface.py
+++ code/UserInterface.py
@@ -1,9 +1,6 @@
-import time
-
 import pygame
 
 from .Configuration import Configuration
-from .Camera import Camera
 
 
 class UserInterface():
@@ -15,11 +12,3 @@ class UserInterface():
         img = pygame.transform.smoothscale(img, (Configuration.SCALE_WIDTH, Configuration.SCREEN_HEIGHT))
         cls.screen.blit(img, ((Configuration.SCREEN_WIDTH - Configuration.SCALE_WIDTH) / 2, 0))
         pygame.display.flip()
-
-    @classmethod
-    def run_self_pi_box(cls):
-        while True:
-            Camera.wait_for_event()
-            UserInterface.display_image(file=Configuration.EVENT_IMAGE)
-            time.sleep(0.2)
-            Camera.take_picture()
